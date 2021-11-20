0a6a5b315ec970829a9beacf4e524797ede24b41
Mingsicogi <codedoctor119@gmail.com>
2021-11-20 22:48:00 +0900

    go study about theory

--- theory/main.go
+++ theory/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/Mingsicogi/go_study/theory/utils"
+	"strings"
 )
 
 func main() {
@@ -13,6 +14,20 @@ func main() {
 
 	//const name string = "minssogi"
 	name := "minssogi"
-	fmt.Print(name)
+	fmt.Println(name)
 
+	fmt.Println(multiply(200, 4))
+
+	lengthOfName, upperCaseOfName := lengthAndUpperStr("minssogi")
+	fmt.Println(lengthOfName, upperCaseOfName)
+}
+
+// simple function
+func multiply(a int, b int) int {
+	return a * b
+}
+
+// multiple return
+func lengthAndUpperStr(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
 }
