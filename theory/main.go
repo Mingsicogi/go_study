948464e41b9cbff4c56c15b51fae076c28934418
Mingsicogi <codedoctor119@gmail.com>
2021-11-20 19:09:25 +0900

    go study about theory

---
+++ theory/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Mingsicogi/go_study/theory/utils"
+)
+
+func main() {
+	fmt.Print("Hello Go!")
+	utils.SayBye()
+
+	println()
+
+	//const name string = "minssogi"
+	name := "minssogi"
+	fmt.Print(name)
+
+}
---
+++ theory/utils/common_util.go
@@ -0,0 +1,11 @@
+package utils
+
+import "fmt"
+
+func SayBye() {
+	fmt.Print("Bye~!")
+}
+
+func sayHello() {
+	fmt.Print("Hello~!")
+}
