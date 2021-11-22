4a65600c7ea5cadef22a56e14410bf52a4fdd24f
Mingsicogi <codedoctor119@gmail.com>
2021-11-23 01:35:06 +0900

    go study about practice  - dictionary add method

--- practice/dictionary/main.go
+++ practice/dictionary/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	dictionary := mydict.Dictionary{"first": "first world"}
 
+	// search
 	value, err := dictionary.Search("second")
 	if err == nil {
 		fmt.Println(value)
@@ -19,6 +20,21 @@ func main() {
 	if err == nil {
 		fmt.Println(value)
 	} else {
+		fmt.Println(err)
+	}
+
+	// add
+	err = dictionary.Add("second", "bye world")
+	if err != nil {
+		fmt.Println(err)
+	}
+	value, err = dictionary.Search("second")
+	if err == nil {
+		fmt.Println(value)
+	}
+
+	err = dictionary.Add("second", "bye world")
+	if err != nil {
 		fmt.Println(err)
 	}
 }
--- practice/dictionary/mydict/mydict.go
+++ practice/dictionary/mydict/mydict.go
@@ -4,11 +4,25 @@ import "errors"
 
 type Dictionary map[string]string
 
+var NotFoundData = errors.New("Not found data")
+
 func (d Dictionary) Search(key string) (string, error) {
 	value, exists := d[key]
 	if exists {
 		return value, nil
 	} else {
-		return "", errors.New("Not found data")
+		return "", NotFoundData
+	}
+}
+
+func (d Dictionary) Add(key string, value string) error {
+	_, err := d.Search(key)
+	if err != nil {
+		if err == NotFoundData {
+			d[key] = value
+		}
+		return nil
+	} else {
+		return errors.New("Already exists")
 	}
 }
